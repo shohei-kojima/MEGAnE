517c215bcf4925f09833fad882d1e63896e87560
shohei-kojima <26kojima@gmail.com>
2020-05-31 18:15:40 +0900

    20200531-181540

--- helper_scripts/calc_mapping_depth.py
+++ helper_scripts/calc_mapping_depth.py
@@ -12,10 +12,10 @@ Usage: python calc_mapping_depth.py -i samtools_out.txt -o out.txt
 
 Exmaple:
 samtools coverage --reference ref.fa in.cram > in.txt
-python calc_mapping_depth.py -i in.txt -o out.txt
+python calc_mapping_depth.py -i in.txt -chr /path/to/prog/lib/human_autosomes_ucsc_stype.txt -o out.txt
 
 Usage details:
-Before using this script, you need to run 'samtools coverage' to calculate depth of each chromosome. You need to use samtools 1.10 or later to use 'coverage' function. The output from 'samtools coverage' contains mean depth of each chromosome. This program takes the output file from 'samtools coverage' to calculate mean depth of all chromosomes. You can specify the output file with '-i' option.
+Before using this script, you need to run 'samtools coverage' to calculate depth of each chromosome. You need to use samtools 1.10 or later to use 'coverage' function. The output from 'samtools coverage' contains mean depth of each chromosome. This program takes the output file from 'samtools coverage' to calculate mean depth of autosomes. You can specify the output file with '-i' option. You can specify names of autosomes by specifying a file containing names of autosomes with -chr option.
 
 Show help message:
 python calc_mapping_depth.py -h
@@ -25,6 +25,7 @@ import os,sys,argparse
 
 parser=argparse.ArgumentParser(description='')
 parser.add_argument('-i', metavar='str', type=str, help='Required. Specify output file from samtools coverage.')
+parser.add_argument('-chr', metavar='str', type=str, help='Required. Specify a file containing autosomes. Default=/path/to/prog/lib/human_autosomes_ucsc_stype.txt')
 parser.add_argument('-o', metavar='str', type=str, help='Optional. Specify output file name. Otherwise, output result as stdout.')
 parser.add_argument('-overwrite', help='Optional. Specify if you overwrite previous results.', action='store_true')
 args=parser.parse_args()
@@ -39,6 +40,14 @@ else:
         print('Error: Input file does not exist.')
         exit(1)
 
+if args.chr is None:
+    base=os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+    args.chr=os.path.join(base, '../lib/human_autosomes_ucsc_stype.txt')
+else:
+    if os.path.exists(args.chr) is not True:
+        print('Error: Chromosome file do not exists.')
+        exit(1)
+    
 if args.o is not None:
     if os.path.exists(args.o) is True:
         if args.overwrite is False:
@@ -49,19 +58,27 @@ if args.o is not None:
 
 
 # main
+chrs=set()
+with open(args.chr) as infile:
+    for line in infile:
+        chrs.add(line.strip())
+
 genome_len=0
 cov_dep=0
+chrs_used=[]
 with open(args.i) as infile:
     for line in infile:
         if not line[0] == '#':
             ls=line.strip().split('\t')
-            genome_len += int(ls[4])
-            cov_dep += int(ls[4]) * float(ls[6])
+            if ls[0] in chrs:
+                genome_len += int(ls[4])
+                cov_dep += int(ls[4]) * float(ls[6])
+                chrs_used.append(ls[0])
 mean_dep= cov_dep / genome_len
 mean_dep= round(mean_dep)
 
 if args.o is not None:
     with open(args.o, 'w') as outfile:
-        outfile.write('%d\t%s\n' % (mean_dep, os.path.abspath(args.i)))
+        outfile.write('%d\t%s\t%s\n' % (mean_dep, os.path.abspath(args.i), ';'.join(chrs_used)))
 else:
-    print('%d\t%s' % (mean_dep, os.path.abspath(args.i)))
+    print('%d\t%s\t%s' % (mean_dep, os.path.abspath(args.i), ';'.join(chrs_used)))
